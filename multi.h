f818f94381b91c43741d54d5a2237cd8ef056619
jwu2018 <jyalu.wu@hotmail.com>
2020-01-29 19:21:52 -0500

    background

---
+++ multi.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int LINE_MAX = 128;
+
+int main(int argc, char *argv[]);
+void execute(char* command, char** currentDir_ptr, int lineNum, 
+	int* bgIndex_ptr, int** bg_ptr);
+void  parse(char *line, char **argv);
+void changeDir(char** newDir_ptr);
+void printDir(char** currentDir_ptr);
+
+#endif
